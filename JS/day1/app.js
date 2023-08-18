fe13af2aae593607f9ee293b9450942af59b303f
meet-navapara <meetnavapara1@gmail.com>
2023-08-18 17:18:39 +0530

    day2 added

--- JS/day1/app.js
+++ JS/day1/app.js
@@ -4,14 +4,14 @@
 // &
 // *
 // ~
+// ^
 
 var b =10
 // let i = 1;
 for (let i=0;i<=b;i++){
     console.log(i); 
 }
-
-// function btn()
-// {
-//     alert("hello")
-// }
+function btn()
+{
+    alert("hello")
+}
---
+++ JS/day2/app.js
@@ -0,0 +1,20 @@
+// ^ variable in js
+//? 1 : var
+//? 2: const
+//? 3: let
+
+
+//* 1: var.................................
+
+//! declaration
+var a;
+//! initialization
+a=10;
+console.log(a);
+//! re-initialization
+a=100;
+console.log(a);
+//! re-declaration & re-initialization
+var a;
+a=200;
+console.log(a);
