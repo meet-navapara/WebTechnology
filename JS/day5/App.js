c28d37aea02c06d6fe1ef986afa6d45333dbb946
meet-navapara <meetnavapara1@gmail.com>
2023-09-09 14:53:49 +0530

    09-09

--- JS/day5/App.js
+++ JS/day5/App.js
@@ -1,62 +1,75 @@
 //! datatypes
 
 
-//* number
-let a = 10
-console.log(a);
+// //* number
+// let a = 10
+// console.log(a);
 
-//* string
-let b = "hello"
-console.log(b);
+// //* string
+// let b = "hello"
+// console.log(b);
 
-//* boolean
-let c = true;
-console.log(c);
+// //* boolean
+// let c = true;
+// console.log(c);
 
-//* Bigint
-let d =111111111111111111111111111111111n
-console.log(d);
+// //* Bigint
+// let d =111111111111111111111111111111111n
+// console.log(d);
 
-//* undefined
-let f= undefined
-console.log(f);
+// //* undefined
+// let f= undefined
+// console.log(f);
 
-//* null
-let g = null
-console.log(g);
+// //* null
+// let g = null
+// console.log(g);
 
-//*symbol
-let h = Symbol(a);
-console.log(h);
-
-
-
-//& examples
-console.log("hello");
-console.log(10-"12"-"12");
-console.log(9 -"9" +9);
-console.log(window);
-console.log(typeof 3 ); 
-console.log(typeof "hello");
-console.log(typeof 1*3);
-console.log("10"+"hello");
-console.log(typeof window);
-console.log(typeof this);
-console.log(typeof String());
-console.log(Date());
+// //*symbol
+// // let h = Symbol(a);
+// // console.log(h);
 
 
-//? prove that window and this keyword both are same 
-var ab = window == this
 
-console.log(ab);
-
-console.log(window);
-console.log(this);
-
-// var first = Number(prompt("enter value"))
-// console.log(first);
-
-
-console.log("3"+ + 3);
+// //& examples
+// console.log("hello");
+console.log(1+ +'3'+'2');
+// console.log('9'-"hi");
+console.log('3' + +3);
 console.log(3 + + "3");
+// console.log(window);x
+// console.log(typeof 3 ); 
+// console.log(typeof "hello");
+// console.log(typeof 1*3);
+// console.log("10"+"hello");
+// console.log(typeof window);
+// console.log(typeof this);
+// console.log(typeof String());
+// console.log(Date());
+
+
+// //? prove that window and this keyword both are same 
+// // var ab = window == this
+
+// // console.log(ab);
+
+// // console.log(window);
+// // console.log(this);
+
+// // var first = Number(prompt("enter value"))
+// // console.log(first);
+
+
+// console.log("3"+ + 3);
+// console.log(3 + + "3");
+
+
+
+// var a= 21;
+var girl = function()
+{
+    console.log(a);
+    //  var a = 20;
+}
+girl()
+let a=10;
--- JS/day6/App.js
+++ JS/day6/App.js
@@ -1,23 +1,128 @@
-console.log("Hello");
+// // "use strict"
+// console.log("Hello");
+// document.write("Scopes")
+// abc=10
+// var let = 10
 
-var a = 10;
-console.log(a);
+// var a = 10;
+// console.log(a);
 
-let b =20;
-console.log(b);
+// let b =20;
+// console.log(b);
 
 
 
-var aa = 10
-let bb = 11
-const cc = 13
-//* block scope start
-{
-    console.log(aa);
-    console.log(bb);
-    console.log(cc);
-    let ab= 10;
-    console.log(typeof ab);
+// var aa = 10
+// let bb = 11
+// const cc = 13
+// //* block scope start
+// {
+//     console.log(aa);
+//     console.log(bb);
+//     console.log(cc);
+//      ab= 10;
+   
+// }
+// //* block scope ends 
+
+// //* function scope starts
+// function demo()
+// {
+//     console.log(aa);
+//     console.log(bb);
+//     console.log(cc);
+//     var d1=10
+//     let d2=30
+//     const d3=35
+//     console.log(d1);
+// }
+// demo()
+// // console.log(ab);
+
+// x=0;
+// y=0;
+// x = x + ++x - ++x + --x +y//? 0
+
+// y = --y - ++x - ++y - x++ + x++ - x++ -y ;//? -4
+// x = --x - ++x + --y + ++x -y +x +1 + --y;//? 0
+// y = x + y + ++y + y++ - x++ + --y  + 2 + x++;//? 6
+// x = -y - x-- + x++ -x++ - ++y - ++x - --y;//? -4
+// x = --x + ++x - --y - ++x + y - x -1 - --y//? -2
+// console.log(y + x + ++x + x++ + --x + 2 + --y);//? 4
+// console.log(x + y++ - y-- + y-- + --x + --y + ++x);//? -4
+// console.log(x);
+
+
+
+// let i;
+// for (i=1;i<=90;i++)
+// {
+//     if(90%i===0)
+//     {
+//         console.log(i);
+//     }
+// }
+// let i;
+// for (i=1;i<=90;i++)
+// {
+//     if(i%3===0)
+//     {
+//         console.log(i);
+//     }
+// }
+
+// let i;
+// let count = 1;
+// for (i=9;i>=1;i--)
+// {
+//     count = count*i;
+// }
+// console.log(count);
+
+// let count = 0;
+// let number = Number(prompt("enter value"))
+// let i;
+// for (i = 0; i<=number ; i++)
+// {
+//     if(number%i === 0)
+//     {
+//         count++;
+//     }
+// }
+// if(count===2)
+// {
+// console.log("prime number");
+// }
+// else
+// console.log("not a prime");
+
+
+// function multi(a = 0,b = 0)
+// {
+//     for(let i = 1;i<=b;i++)
+//     {
+//         if(i%a===0 && i%(a*2)==0)
+//         {
+//             console.log(i);
+//         }
+//     }
+// }
+// multi(5,200)
+
+ 
+// function fect(a=0)
+// {
+//     let count=1;
+//     for(let i = 1;i<=a;i++)
+//     {
+//         if(count<=3){
+//         if(a%i==0)
+//         {
+            
+//             console.log(i);
+//             count++;
+//         }
+//     }
+// }
 }
-//* block scope ends 
-// console.log(ab);
+fect(50);
